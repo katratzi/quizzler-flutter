56f30a6c24eafc01d2e3b5bfc97a600c44e52957
Richard Clifford <richard.clifford@soluis.com>
2020-04-16 21:01:24 +0100

    create a list to hold the scores. small function for building the icon

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,19 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.close, color: Colors.red),
+    Icon(Icons.close, color: Colors.red),
+  ];
+
+  /// Build a little Icon widget with the right icon and color based on if the answer was [correct].
+  Icon answer(bool correct) {
+    return Icon(correct ? Icons.check : Icons.close,
+        color: correct ? Colors.green : Colors.red);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -61,7 +74,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  scoreKeeper.add(answer(true));
+                });
               },
             ),
           ),
@@ -80,11 +95,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  scoreKeeper.add(answer(false));
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
